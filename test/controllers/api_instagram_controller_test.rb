298e654f35e425933eb4592895668d42b38da790
Francisco <fagarri@uc.cl>
2016-05-15 22:19:33 +0000

    coverall

--- test/controllers/api_instagram_controller_test.rb
+++ test/controllers/api_instagram_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ApiInstagramControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+     assert true
+  end
 end
